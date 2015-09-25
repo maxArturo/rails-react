8774f6899ee34ef91975196e66d0ae4d581dee46
Max Alcala <maxarturo@gmail.com>
2015-09-25 18:42:37 -0500

    add haml, react, add comments resource

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'comments#index'
+
+  resources 'comments', only: 'index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
